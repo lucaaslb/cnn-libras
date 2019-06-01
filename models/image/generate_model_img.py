f32a6476b6a21f60ca98e18f36ec2d4a1c404829
Lucas Lacerda <lucaaslb@hotmail.com>
2019-06-01 17:42:01 -0300

    gerar imagem do modelo treinado

--- models/image/generate_model_img.py
+++ models/image/generate_model_img.py
@@ -1,6 +1,8 @@
 from keras.utils import to_categorical, plot_model 
 from keras.models import load_model
 
-model = load_model('models/cnn_model_LIBRAS_20190528_001136.h5')
 
-plot_model(model, to_file='models/image/1.png', show_shapes = True)
+model_name = ''
+model = load_model('models/'+ model_name)
+
+plot_model(model, to_file='models/image/'+model_name'.png', show_shapes = True)
