a4bcd07c480890e1b5290b5093fdb0e858f32324
Lucas Lacerda <lucaaslb@hotmail.com>
2019-06-09 13:40:30 -0300

    script para obter imagem e sumario do modelo treinado

--- models/image/generate_model_img.py
+++ models/image/generate_model_img.py
@@ -1,8 +1,24 @@
+'''
+@author Lucas Lacerda
+
+Gerar imagem e sumario do modelo treinado
+
+@param local e nome do arquivo de model
+@param nome para salvar a imagem
+
+'''
+
 from keras.utils import to_categorical, plot_model 
 from keras.models import load_model
+import sys
 
+model = sys.argv[1]
+model = load_model(model)
 
-model_name = ''
-model = load_model('models/'+ model_name)
+model_name = sys.argv[2]
 
-plot_model(model, to_file='models/image/'+model_name'.png', show_shapes = True)
+plot_model(model, to_file=model_name+'.png', show_shapes = True)
+
+print('[INFO] Summary: ')
+
+model.summary()
